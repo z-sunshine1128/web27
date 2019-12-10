2a2e5802e1310b977083a105761101e9be2ad80d
liuxinkai <k13044492542@163.com>
2019-12-10 17:17:41 +0800

    刘鑫锴

---
+++ liuxinkai/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LiuxinkaiConfig(AppConfig):
+    name = 'liuxinkai'
--- web27/urls.py
+++ web27/urls.py
@@ -35,4 +35,5 @@ urlpatterns = [
     path('zhangyilin/', include('zhangyilin.urls')),
     path('qiuyuxin/',include('qiuyuxin.urls')),
     path('zhoupengbo/',include('zhoupengbo.urls')),
+    path("liuxinkai/",include('liuxinkai.urls'))
 ]
