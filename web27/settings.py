d81cb12d28be6328c6e548d5079b1f0e7939b3c4
张以琳 <1246253508@qq.com>
2019-12-10 16:38:54 +0800

    张以琳第二次提交

--- web27/settings.py
+++ web27/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'xuecuiping',
     'liuyajing',
     'mateng',
+    'zhangyilin'
 ]
 
 MIDDLEWARE = [
---
+++ zhangyilin/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ZhangyilinConfig(AppConfig):
+    name = 'zhangyilin'
